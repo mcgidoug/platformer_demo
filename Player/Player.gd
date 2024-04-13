7c1d9c36d5846d8dacda667fcfe31d279125e0a6
Doug McGillivray <mcgidoug@gmail.com>
2024-04-13 15:11:18 -0500

    Added animation sprites

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim = get_node("AnimatedSprite2D")
+
 
 func _physics_process(delta):
 	# Add the gravity.
